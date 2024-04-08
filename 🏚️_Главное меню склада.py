54d4a017e3215af7de32bcbe67d0c4ec2acdaba2
TheDeaX-Lab <Aleksandr>
2024-04-08 22:05:07 +0300

    initial commit

---
+++ 🏚️_Главное меню склада.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Склад",
+    page_icon="🏚️",
+)
+
+st.write("# Добро пожаловать в Мегасклад")
+
+st.title("Карта склада")
+st.image("Rezultativnost-sostavlenija-temperaturnoj-karty.jpg")
